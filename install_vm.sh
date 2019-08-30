34517bc8159cf0ec710d8c85b8029acd43fb3b38
Titov Petr <petr@dntplus.ru>
2019-08-30 09:58:20 +0300

    add grub

--- install_vm.sh
+++ install_vm.sh
@@ -1,22 +1,26 @@
 #!/bin/sh
 
-HOSTNAME_VM="ol_jessie"
-DISTRIB="jessie"
+HOSTNAME_VM="test5"
+
+DISTRIB="buster"
 LVM_VG="work"
-REPO_URL="http://repo.pet4.ru:9999/mirror.yandex.ru/debian"
+REPO_URL="http://httpredir.debian.org/debian"
 
 DISK_SIZE="5G"
-MEM_MB="1024"
-VCPUS="3"
+MEM_MB="2048"
 INT_BR="br.0"
+V_CPU="2"
+V_CHIPSET='pc-i440fx-2.8'
 
+FS_TYPE="ext4"
 ROOTFS_LABEL="${HOSTNAME_VM}-root"
 ROOT_DIR="/tmp/vm_install/${HOSTNAME_VM}"
 INSTALL_DIR="${ROOT_DIR}/rootfs"
 BOOT_DIR="${INSTALL_DIR}/boot"
 VM_BOOT_DIR="/var/lib/libvirt/boot/${HOSTNAME_VM}"
-DEVICE_NAME="/dev/${LVM_VG}/${ROOTFS_LABEL}"
+LVM_NAME="/dev/${LVM_VG}/${ROOTFS_LABEL}"
 MEM_KB="$((${MEM_MB}*1024))"
+DEVICE_NAME="/tmp/virtual_disk"
 
 die_script(){
    echo "$1"
@@ -24,8 +28,6 @@ die_script(){
 }
 
 configuring_vm(){
-
-  mount -B ${VM_BOOT_DIR} ${BOOT_DIR}
 
   # mount special fs
   chroot ${INSTALL_DIR} bash -c 'mount -t proc none /proc'
@@ -45,12 +47,11 @@ configuring_vm(){
     return 1
   fi
 
-  printf "LABEL=${ROOTFS_LABEL} / ext4 defaults 0 0\n" >> ${INSTALL_DIR}/etc/fstab
-  printf "boot /boot 9p defaults 0 0\n" >> ${INSTALL_DIR}/etc/fstab
+  printf "LABEL=${ROOTFS_LABEL} / ${FS_TYPE} defaults 0 0\n" >> ${INSTALL_DIR}/etc/fstab
   printf "${HOSTNAME_VM}" > ${INSTALL_DIR}/etc/hostname
   
   # change root password
-  chroot ${INSTALL_DIR} bash -c 'echo -e "123\n123\n"|passwd'
+  chroot ${INSTALL_DIR} bash -c 'passwd -d root'
 
   # configuring tzdata
   chroot ${INSTALL_DIR} bash -c 'ln -fs /usr/share/zoneinfo/Europe/Moscow /etc/localtime'
@@ -72,12 +73,8 @@ configuring_vm(){
   chroot ${INSTALL_DIR} bash -c 'locale-gen'
   printf 'LANG=C.UTF-8\n' >> ${INSTALL_DIR}/etc/default/locale
 
-  printf 'do_symlinks = yes\nlink_in_boot = yes' >> ${INSTALL_DIR}/etc/kernel-img.conf
-
   # install kernel
   chroot ${INSTALL_DIR} bash -c 'apt-get update && apt-get install -y linux-image-amd64' 
-  printf '9p\n9pnet\n9pnet_virtio' >>  ${INSTALL_DIR}/etc/initramfs-tools/modules
-  chroot ${INSTALL_DIR} bash -c 'update-initramfs -u' 
 
   if [ "$?" -ne "0" ]
   then
@@ -86,8 +83,34 @@ configuring_vm(){
     return 1
   fi
 
-  return 0
+  # install grub
+  chroot ${INSTALL_DIR} bash -c 'DEBIAN_FRONTEND=noninteractive apt-get install -y grub2' 
+  
+  if [ "$?" -ne "0" ]
+    then
+      umount ${INSTALL_DIR}/proc ${INSTALL_DIR}/sys
+      echo 'ERROR: Can not install package grub2.'
+      return 1
+    fi
+  
+    sed -i '/GRUB_CMDLINE_LINUX_DEFAULT/s/quiet/quiet console=ttyS0/' ${INSTALL_DIR}/etc/default/grub
+    printf 'GRUB_TERMINAL="serial console"\n' >> ${INSTALL_DIR}/etc/default/grub
+    printf 'GRUB_SERIAL_COMMAND="serial --unit=0 --speed=115200 --word=8 --parity=no --stop=1"\n' >> ${INSTALL_DIR}/etc/default/grub
 
+    test -e "${INSTALL_DIR}${DEVICE_NAME}" && die_script "ERROR: ${INSTALL_DIR}${DEVICE_NAME} - file exist." || mknod ${INSTALL_DIR}${DEVICE_NAME} b 7 25 
+    chroot ${INSTALL_DIR} bash -c 'update-grub'
+    sed -i "s!${DEVICE_NAME}!LABEL=${ROOTFS_LABEL}!" "${INSTALL_DIR}/boot/grub/grub.cfg"
+    chroot ${INSTALL_DIR} bash -c "grub-install --force ${DEVICE_NAME}"
+    rm ${INSTALL_DIR}${DEVICE_NAME}
+
+   if [ "$?" -ne "0" ]
+   then
+     umount ${INSTALL_DIR}/proc ${INSTALL_DIR}/sys
+     echo 'can not install grub' 
+     return 1
+   fi
+
+   return 0
 }
 
 echo "Create LVM for ${ROOTFS_LABEL}"
@@ -96,43 +119,44 @@ vgs "${LVM_VG}" >/dev/null 2>&1 || die_script 'ERROR: Can not find vg.'
 lvs "${LVM_VG}" 2>&1|grep "^\s\{1,\}${ROOTFS_LABEL}\s" && die_script 'ERROR: LVM volume already exist.'
 lvcreate "${LVM_VG}" --name "${ROOTFS_LABEL}" -L ${DISK_SIZE} || die_script 'ERROR: Can not create lvm.'
 
-echo "Create ext4 on ${DEVICE_NAME}"
-mkfs.ext4 -L ${ROOTFS_LABEL} ${DEVICE_NAME}
+test -e "${DEVICE_NAME}" && die_script "ERROR: ${DEVICE_NAME} - file exist." || mknod ${DEVICE_NAME} b 7 25 
+
+losetup ${DEVICE_NAME} ${LVM_NAME}
+
+echo "Create ${FS_TYPE} on ${DEVICE_NAME}"
+mkfs -t ${FS_TYPE} -L ${ROOTFS_LABEL} ${DEVICE_NAME}
 
 [ -d "${INSTALL_DIR}" ] || mkdir -p "${INSTALL_DIR}"
-[ -d "${VM_BOOT_DIR}" ] || mkdir -p "${VM_BOOT_DIR}"
 
 echo "Install system on ${DEVICE_NAME}"
-mount LABEL=${ROOTFS_LABEL} ${INSTALL_DIR} &&\
+mount ${DEVICE_NAME} ${INSTALL_DIR} &&\
   debootstrap "${DISTRIB}" "${INSTALL_DIR}" "${REPO_URL}" &&\
   configuring_vm 
-
+    
 if [ "$?" -ne "0" ]
 then
-  umount ${INSTALL_DIR} 
+  #umount ${INSTALL_DIR}/${TMP_FLD} ${INSTALL_DIR} && losetup -d "${DEVICE_NAME}" && rm "${DEVICE_NAME}"
+  umount ${INSTALL_DIR} && losetup -d "${DEVICE_NAME}" && rm "${DEVICE_NAME}" "${INSTALL_DIR}${DEVICE_NAME}"
   die_script 'ERROR: Can not configured vm.'
 fi
- 
 
-umount ${INSTALL_DIR}/boot ${INSTALL_DIR}/proc ${INSTALL_DIR}/sys ${INSTALL_DIR}
 
-chown -R libvirt-qemu:libvirt-qemu ${VM_BOOT_DIR}
+umount ${INSTALL_DIR}/proc ${INSTALL_DIR}/sys ${INSTALL_DIR} && losetup -d "${DEVICE_NAME}" && rm "${DEVICE_NAME}"
+
 
 virsh define /dev/stdin <<__EOF__
 <domain type='kvm'>
   <name>${HOSTNAME_VM}</name>
   <memory unit='KiB'>${MEM_KB}</memory>
-  <vcpu placement='static'>${VCPUS}</vcpu>
+  <vcpu placement='static'>${V_CPU}</vcpu>
   <os>
-    <type arch='x86_64' machine='pc-i440fx-2.8'>hvm</type>
-    <kernel>${VM_BOOT_DIR}/vmlinuz</kernel>
-    <initrd>${VM_BOOT_DIR}/initrd.img</initrd>
-    <cmdline>root=LABEL=${ROOTFS_LABEL} ro console=ttyS0 quiet</cmdline>
+    <type arch='x86_64' machine='${V_CHIPSET}'>hvm</type>
+    <boot dev='hd'/>
   </os>
   <features>
     <acpi/>
     <apic/>
-    <vmport state='off'/>
+    <vmport state='off'/> 
   </features>
   <pm>
     <suspend-to-mem enabled='no'/>
@@ -141,7 +165,7 @@ virsh define /dev/stdin <<__EOF__
   <devices>
     <emulator>/usr/bin/kvm</emulator>
     <disk type='block' device='disk'>
-      <source dev='${DEVICE_NAME}'/>
+      <source dev='${LVM_NAME}'/>
       <target dev='vda' bus='virtio'/>
     </disk>
     <interface type='bridge'>
@@ -151,11 +175,6 @@ virsh define /dev/stdin <<__EOF__
       <target type='serial' port='0'/>
     </console>
     <memballoon model='virtio'></memballoon>
-    <filesystem type='mount' accessmode='mapped'>
-      <driver type='path' wrpolicy='immediate'/>
-      <source dir='${VM_BOOT_DIR}'/>
-      <target dir='boot'/>
-    </filesystem>
   </devices>
 </domain>
 __EOF__
